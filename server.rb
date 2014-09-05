6b411456fd2a6febd2ed9e3a6fe7d29d5810e2de
david karapetyan <dkarapetyan@scriptcrafty.com>
2014-09-05 09:57:53 -0700

    golfing optimizations

--- server.rb
+++ server.rb
@@ -7,7 +7,7 @@ $line_count, $log_file = 0, open('/tmp/log' + Time.now.to_s.gsub(' ', '_'), 'a')
 $line_count_mutex, $log_file_mutex = Mutex.new, Mutex.new
 
 # Reset the line count and re-open the file
-$reset = lambda do
+$reset = ->() do
   $log_file_mutex.synchronize do
     $line_count = 0
     $log_file.close
@@ -17,21 +17,16 @@ end
 
 # Read if the client is still sending data and write it to the log file.
 # When we reach the line count re-open the log file and reset the counters.
-$client_handler = lambda do |client|
+$client_handler = ->(client) do
   Thread.new do
-    while (log_line = client.readline.strip)
-      $log_file_mutex.synchronize { $log_file.puts log_line }
-      $line_count_mutex.synchronize do
-        $line_count += 1
-        if $line_count > $line_count_limit then $reset.call end
-      end
+    while (log_line = client.readline)
+      $log_file_mutex.synchronize { $log_file.write log_line }
+      $line_count_mutex.synchronize { ($line_count += 1) > $line_count_limit ? $reset.call : nil }
     end
   end
 end
 
 # Start the server
 UNIXServer.open('/tmp/logger') do |server|
-  while (client = server.accept)
-    $client_handler.call(client)
-  end
+  loop { $client_handler.call(server.accept) }
 end
